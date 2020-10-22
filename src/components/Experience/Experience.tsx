0f1e514bbfb60faf81d3eee02ad3bf329eec24f2
matviiko <matviy.kopchak@gmail.com>
2020-10-22 13:41:11 +0300

    add Experience item

--- src/components/Experience/Experience.tsx
+++ src/components/Experience/Experience.tsx
@@ -7,9 +7,11 @@ export const Experience: React.FC = () => {
             <h2 className="box-title">Experience</h2>
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Enim laboriosam officiis porro, rem soluta tempora.</p>
         </div>
-        <div className="">
-            <h4 className="experience__title">Title</h4>
-            <p>Company</p>
+        <div className="experience__item">
+            <span className="item__title">Title</span>
+            <span>Company</span>
+            <p className="item__date">Apr, 2015 — May, 2015</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam cum doloremque ducimus eius enim facere illo ipsa maiores necessitatibus nesciunt, odit quibusdam repellat repudiandae rerum sed sint sunt tenetur ullam.</p>
         </div>
     </div>
 
